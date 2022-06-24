a16f7369edd09463267978c571d50f97bc74f640
Rupayan Roy <rupayanroy981@gmail.com>
2022-06-24 23:07:00 +0530

    Add solution for problem `A - Display The Number` Link: https://codeforces.com/contest/1295/problem/A Tags: greedy, *900 Ref: https://codeforces.com/contest/1295/submission/161665410

--- codeforces/1295/A.cpp
+++ codeforces/1295/A.cpp
@@ -6,23 +6,20 @@ using namespace std;
 int main(){
     int testcase;
     cin>>testcase;
-
     while(testcase--){
-        int n;//n khana segs use kore max koto number create kora jabe,
-              // n niyechi , setar amra 1(2 segs) and 7(3 segs) digit ta diye dekbo
+        int n;
         string ans;
         cin>>n;
-
-        if(n%2==0){// jodi n even hoy,then 1 print kortei thakbbo until n==0, 
+        if(n%2==0){
             while(n>0){
-                ans+='1';// '1' print korchi manei 2 segs use hoche, 
-                n-=2;//tai total given segs theke 2 kore deduct hoche
+                ans+='1';
+                n-=2;
             }
         }
         else{
-            ans+='7';//jodi n odd hoy,then, initially 7 add kore debo and total segs theke 3 subtract korbo(7 er segs =3)
-            for(int i = 0; i<n-3; i+=2){// er por dekh jemni total(odd) theke 3(odd) minus korlam, even holo and then
-                ans+='1';// abr 1 print kora shuru holo , and n ke 2 kore deduct kora hoche
+            ans+='7';
+            for(int i = 0; i<n-3; i+=2){
+                ans+='1';
             }
         }
         cout<<ans<<endl;
