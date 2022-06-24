a939a0f03dff0f9d1e8bb22edefb3ff836b3ef56
Rupayan Roy <rupayanroy981@gmail.com>
2022-06-24 21:01:00 +0530

    Add solution for problem `A - Visiting a Friend` Link: https://codeforces.com/contest/902/problem/A Tags: greedy, implementation, *1100 Ref: https://codeforces.com/contest/902/submission/161654199

--- codeforces/902/A.cpp
+++ codeforces/902/A.cpp
@@ -11,12 +11,12 @@ int main(){
         int minimum_limit,maximium_limit;
         cin>>minimum_limit>>maximium_limit;
 
-        if(maxi_checker>=minimum_limit){ //Lower limit of current case should be less than equal to the upper limit of the prev max limit
-            maxi_checker = max(maxi_checker,maximium_limit);//update the counter
+        if(maxi_checker>=minimum_limit){
+            maxi_checker = max(maxi_checker,maximium_limit);
         }
     }
 
-    if(maxi_checker==max_limit){//if the max limit given is not less than or greater than the given final limit at the first, then..
+    if(maxi_checker==max_limit){
         cout<<"YES";
     }
     else{
