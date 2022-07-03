a9342d647f5657105aa12a9f890f0f222f98e142
Rupayan Roy <rupayanroy981@gmail.com>
2022-07-04 00:08:00 +0530

    Add solution for problem `A - Sagheer and Crossroads` Link: https://codeforces.com/contest/812/problem/A Tags: implementation, *1200 Ref: https://codeforces.com/contest/812/submission/162673394

--- codeforces/812/A.cpp
+++ codeforces/812/A.cpp
@@ -1,16 +1,57 @@
-#include<iostream>
-#include<cstdio>
+#include<bits/stdc++.h>
 using namespace std;
+
 int main(){
-	int l[10],s[10],r[10],p[10];
-	for(int i=1;i<=4;i++){
-		scanf("%d%d%d%d",&l[i],&s[i],&r[i],&p[i]);
-	}
-	bool flag=false;
-	if(p[1]==1&&((l[1]==1||s[1]==1||r[1]==1)||(l[2]==1||s[3]==1||r[4]==1))) flag=true;
-	if(p[2]==1&&((l[2]==1||s[2]==1||r[2]==1)||(l[3]==1||s[4]==1||r[1]==1))) flag=true;
-	if(p[3]==1&&((l[3]==1||s[3]==1||r[3]==1)||(l[4]==1||s[1]==1||r[2]==1))) flag=true;
-	if(p[4]==1&&((l[4]==1||s[4]==1||r[4]==1)||(l[1]==1||s[2]==1||r[3]==1))) flag=true;
-	if(flag) cout<<"YES"<<endl;
-	else cout<<"NO"<<endl;
+    /*
+    int a,b,c,d,dtots(0);
+    bool flag = false;
+    for(int i = 0; i<4; i++){
+        cin>>a>>b>>c>>d;
+        if(d==1 && a==0 && b==0 && c==0) flag = true;
+        else if(d==1 &&( a!=0 || b!=0 || c!=0)) {
+            cout<<"YES"<<endl;
+            return 0;
+        }
+        dtots+=d;
+    }
+    if(dtots%2!=0 && flag == true) cout<<"NO"<<endl;
+    else if(dtots==0 && flag==false) cout<<"NO"<<endl;
+    else cout<<"YES"<<endl;
+    */
+   int arr[5][5];
+   for(int i = 1; i<=4; i++){
+        for(int j = 1; j<=4; j++){
+            cin>>arr[i][j];
+        }
+   }
+
+   if(arr[1][4]==1){
+        if(arr[1][1]==1 || arr[1][2]==1 || arr[1][3]==1 || arr[3][2]==1 || arr[2][1]==1 || arr[4][3]==1){
+            cout<<"YES"<<endl;
+            return 0;
+        }
+   }
+
+   if(arr[2][4]==1){
+        if(arr[2][1]==1 || arr[2][2]==1 || arr[2][3]==1 || arr[4][2]==1 || arr[1][3]==1 || arr[3][1]==1){
+            cout<<"YES"<<endl;
+            return 0;
+        }
+   }
+
+   if(arr[3][4]==1){
+        if(arr[3][1]==1 || arr[3][2]==1 || arr[3][3]==1 || arr[2][3]==1 || arr[1][2]==1 || arr[4][1]==1){
+            cout<<"YES"<<endl;
+            return 0;
+        }
+   }
+
+   if(arr[4][4]==1){
+        if(arr[4][1]==1 || arr[4][2]==1 || arr[4][3]==1 || arr[3][3]==1 || arr[2][2]==1 || arr[1][1]==1){
+            cout<<"YES"<<endl;
+            return 0;
+        }
+   }
+
+   cout<<"NO"<<endl;
 }
