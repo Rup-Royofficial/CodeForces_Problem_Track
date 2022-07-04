08d5e01c27b5b812e4a7fc21fe4ee9ecb2c7fdeb
Rupayan Roy <rupayanroy981@gmail.com>
2022-07-04 20:20:00 +0530

    Add solution for problem `A - The Third Three Number Problem` Link: https://codeforces.com/contest/1699/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1699/submission/162768993

---
+++ codeforces/1699/A.cpp
@@ -0,0 +1,48 @@
+//░░░░░░░░░░▄
+//░░░░░░░░▄▐░▄▄█████▄▄
+//░░░░░░▄█████████████▄▀▄▄░▄▄▄
+//░░░░░█████████████████▄██████
+//░░░░████▐█████▌████████▌█████▌
+//░░░████▌█████▌█░████████▐▀██▀
+//░▄█████░█████▌░█░▀██████▌█▄▄▀▄
+//░▌███▌█░▐███▌▌░░▄▄░▌█▌███▐███░▀
+//▐░▐██░░▄▄▐▀█░░░▐▄█▀▌█▐███▐█
+//░░███░▌▄█▌░░▀░░▀██░░▀██████▌
+//░░░▀█▌▀██▀░▄░░░░░░░░░███▐███
+//░░░░██▌░░░░░░░░░░░░░▐███████▌
+//░░░░███░░░░░▀█▀░░░░░▐██▐███▀▌
+//░░░░▌█▌█▄░░░░░░░░░▄▄████▀░▀
+//░░░░░░█▀██▄▄▄░▄▄▀▀▒█▀█░▀
+//░░░░░░░░░▀░▀█▀▌▒▒▒░▐▄▄
+//░░░░░░░░▄▄▀▀▄░░░░░░▄▀░▀▀▄▄
+//░░░░░░▄▀░▄▀▄░▌░░░▄▀░░░░░░▄▀▀▄
+//░░░░░▐▒▄▀▄▀░▌▀▄▄▀░░░░░░▄▀▒▒▒▐
+//░░░░▐▒▒▌▀▄░░░░░▌░░░░▄▄▀▒▐▒▒▒▒▌
+//░░░▐▒▒▐░▌░▀▄░░▄▀▀▄▀▀▒▌▒▐▒▒▒▒▐▐
+//░░░▌▄▀░░░▄▀░█▀▒▒▒▒▀▄▒▌▐▒▒▒▒▒▌▌
+//░░▄▀▒▐░▄▀░░░▌▒▐▒▐▒▒▒▌▀▒▒▒▒▒▐▒▌
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int size;
+        cin>>size;
+        if(size%2!=0) {cout<<-1<<endl;}
+        else{ cout<<0<<" "<<0<<" "<<size/2<<endl;}
+    }
+}
+
+/*--------------------------------------------------------------------------------------------------------------------------*/
+ 
+//You have struggled your way here with no guidance from anyone,keep the faith
+//You have practiced a lot,have faith in yourself
+//You have practiced a hell lot of questions,have faith in yourself
+//If nothing strikes for a period of time,calm down,drink water and think again from the start
+//Dont just stare at the problem use that damn pen and paper,write down whats in ur head
+ 
+//AFTER THE WAR AND STRUGGLE,PEACE RESIDES,WORK HARD TODAY FOR THE BETTER TOMORROW
