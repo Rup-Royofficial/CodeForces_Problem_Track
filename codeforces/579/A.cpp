f8e45b09e081b7658a87eca887924371aa247f28
Rupayan Roy <rupayanroy981@gmail.com>
2022-07-06 15:51:00 +0530

    Add solution for problem `A - Raising Bacteria` Link: https://codeforces.com/contest/579/problem/A Tags: bitmasks, *1000 Ref: https://codeforces.com/contest/579/submission/162988502

--- codeforces/579/A.cpp
+++ codeforces/579/A.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 int main(){
-    /*
     long long int counter(0),num;
     cin>>num;
     string str;
@@ -11,16 +10,5 @@ int main(){
         num>>=1;
     }
 
-    cout<<counter<<endl;
-    */
-
-   //logu will store the log of the number
-    int num,logu,counter(0);
-    cin>>num;
-    logu = (int) log2(num);//converting the number to log2
-    string bin = bitset<64>(num).to_string().substr(64-logu-1);//binary representation/conversion of the input number
-    for(int i = 0; i < bin.length(); i++){
-        if(bin[i] == '1')   counter++;
-    }
     cout<<counter<<endl;
 }
